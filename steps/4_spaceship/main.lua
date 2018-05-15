5aff3bf0a735478d593857277d4871d2de7b8b4d
Andros <andros@fenollosa.email>
2018-05-15 16:07:59 +0200

    add spaceshipt

---
+++ steps/4_spaceship/main.lua
@@ -0,0 +1,31 @@
+function love.load()
+	game = {}
+	game.width = 800
+	game.height = 480
+	love.window.setMode(game.width, game.height)
+	score = { x = 500, y = 40 }
+	-- Background
+	background = {}
+	background.img = love.graphics.newImage('assets/sprites/background.jpg')
+	background.x = 0
+	background.y = 0
+	-- Spaceship
+	spaceship = {}
+	spaceship.img = love.graphics.newImage('assets/sprites/spaceship.png')
+	spaceship.x = game.height / 8
+	spaceship.y = (game.height / 4) - (spaceship.img:getHeight() / 2)
+end
+
+function love.draw()
+	-- Background
+	love.graphics.draw(background.img, background.x, background.y)
+	-- Spaceship
+	love.graphics.draw(spaceship.img, spaceship.x, spaceship.y)
+end
+
+-- Controls
+function love.keypressed(key, scancode, isrepeat)
+	if key == 'escape' then
+		love.event.push('quit')
+	end
+end
